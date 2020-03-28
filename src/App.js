1741245b4a754b3788d64a36aae442c6bc702594
Gingertonic <bethmschofield@gmail.com>
2020-03-28 12:35:03 +0000

    basic meme create

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ currentMeme, setCurrentMeme ] = useState('https://imgflip.com/s/meme/Change-My-Mind.jpg')
+  const [ memeFormText, setMemeFormText ] = useState('')
+
+  const handleInputChange = e => setMemeFormText(e.target.value)
+
+  const submitMeme = e => {
+      e.preventDefault();
+      const params = {
+          template_id: 129242436,
+          text0: memeFormText,
+          text1: '',
+          username: process.env.REACT_APP_IMGFLIP_USERNAME,
+          password: process.env.REACT_APP_IMGFLIP_PASSWORD
+      }
+
+      const formData = createFormData(params)
+
+      const config = {
+          body: formData,
+          method: "POST"
+      }
+
+      fetch('https://api.imgflip.com/caption_image', config)
+          .then(res => res.json())
+          .then(updateMeme)
+  }
+
+  const updateMeme = image => {
+    setCurrentMeme(image.data.url)
+  }
+
+  const createFormData = object => {
+      const formData = new FormData();
+      Object.keys(object).forEach(key => formData.append(key, object[key]));
+      return formData;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div id="header">
+            <h1>Change My Mind, Netlify!</h1>
+        </div>
+        <div id="main">   
+            <div id="content" className="meme-generator">
+                <form onSubmit={submitMeme} id="meme-text-form"><input type="text" onChange={handleInputChange} placeholder="Meme Text!"/></form>
+                <img id="meme" src={currentMeme} alt="Change My Mind"/>
+            </div>
+        </div>
     </div>
   );
 }
